12624930b10ce4be6945555cca29f133faf0f5cd
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-09-22 23:13:26 -0300

    Adicionando arquivo gitignore e main.py

---
+++ src/main.py
@@ -0,0 +1,28 @@
+"""
+Lista de exercicios do curso Programação em Python do básico ao avançado.
+
+
+1 - Faça um programa que leia um número inteiro e o imprima.
+
+num = int(input('digite um número inteiro'))
+print(num)
+
+2 - Faça um programa que leia um número real e o imprima.
+
+num = float(input('digite um número inteiro'))
+print(num)
+
+3 - Peça ao usuario para digitar tres valores inteiros e imprima a soma deles.
+
+n1 = int(input('Digite o primeiro número'))
+n2 = int(input('Digite o segundo número'))
+n3 = int(input('Digite o terceiro número'))
+print(n1 + n2 + n3)
+
+4 - Leia um numero real e imprima o resultado do quadrado desse numero.
+
+num = float(input('Digite um número real'))
+quadrado = num ** 2
+print(quadrado)
+
+"""
