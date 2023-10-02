dcb8877009fd0c5bd39a38fdcaca3d362313383e
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-10-02 09:05:25 -0300

    Resolvendo até o exercicio 20

--- src/secaoseis.py
+++ src/secaoseis.py
@@ -103,5 +103,93 @@ print(soma)
 
 11)
 
-"""
+num = int(input('Digite um numero inteiro positivo: '))
 
+for n in range(0, num + 1):
+    print(n)
+
+12)
+
+num = int(input('Digite um numero inteiro positivo: '))
+
+for n in range(num, 0, -1):
+    print(n)
+
+13)
+
+num = int(input('Digite um numero inteiro positivo par: '))
+
+for n in range(0, num + 1):
+    if n % 2 == 0:
+        print(n)
+
+14)
+
+num = int(input('Digite um numero inteiro positivo par: '))
+
+for n in range(num, -1, -1):
+    if n % 2 == 0:
+        print(n)
+
+15)
+
+num = int(input('Digite um numero inteiro positivo impar: '))
+if num % 2 == 1:
+    for n in range(1, num + 1):
+        if n % 2 == 1:
+            print(n)
+else:
+    print('Não digitou um número impar')
+
+16)
+
+num = int(input('Digite um numero inteiro positivo impar: '))
+if num % 2 == 1:
+    for n in range(num, -1, -1):
+        if n % 2 == 1:
+            print(n)
+else:
+    print('Não digitou um número impar')
+
+17)
+
+soma = 0
+num = int(input('Digite um numero inteiro positivo: '))
+
+for n in range(0, num + 1):
+    soma += n
+print(soma)
+
+18)
+
+lista = []
+qtd = int(input('Quantos números voce quer ler?: '))
+for vezes in range(1, qtd + 1):
+    num = int(input('Digite um número?'))
+    lista.append(num)
+print(f'O mair número que voce digitou foi {max(lista)}, e ele apareceu {lista.count(max(lista))} vezes')
+
+19)
+
+num = input('Digite um numero inteiro entre 100 e 999: ')
+
+print(num[0])
+print(num[1])
+print(num[2])
+
+20)
+
+num = 0
+count_total = 0
+count_pares = 0
+while num != 1000:
+    n = int(input('Digite um numero: '))
+    count_total += 1
+    if n % 2 == 0:
+        count_pares += 1
+    num = n
+print(f'Foi lido {count_total - 1} dados e {count_pares - 1} são pares.')
+
+21)
+
+"""
