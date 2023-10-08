dcdf093151b5a1738fa31aab13c2fb3a2063ad0b
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-10-08 12:21:24 -0300

    Resolvendo 52

--- src/secaoseis.py
+++ src/secaoseis.py
@@ -685,7 +685,34 @@ print(f'O salário em {ano - 1} será de {salario}')
 
 52)
 
+saque = int(input('Quanto voce deseja sacar? '))
+
+notas_cem = saque // 100
+resto_cem = saque % 100
+notas_cinquenta = resto_cem // 50
+resto_cinquenta = resto_cem % 50
+notas_vinte = resto_cinquenta // 20
+resto_vinte = resto_cinquenta % 20
+notas_dez = resto_vinte // 10
+resto_dez = resto_vinte % 10
+notas_cinco = resto_dez // 5
+resto_cinco = resto_dez % 5
+notas_dois = resto_cinco // 2
+resto_dois = resto_cinco % 2
+notas_um = resto_dois // 1
+resto_um = resto_dois % 1
+
+print(fPara sacar R${saque}, será necessário as seguintes notas:
+{notas_cem} notas de 100
+{notas_cinquenta} notas de 50
+{notas_vinte} notas de 20
+{notas_dez} notas de 10
+{notas_cinco} notas de 5
+{notas_dois} notas de 2
+{notas_um} notas de 1)
+
+53)
+
 
 
 """
-
