3fbe5ca2e41c0f5622089d1660745fb1af4de4ba
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-10-09 23:18:24 -0300

    Resolvendo 56

--- src/secaoseis.py
+++ src/secaoseis.py
@@ -764,7 +764,28 @@ else:
 
 56)
 
+def eh_primo(numero):
+    if numero <= 1:
+        return False
+    if numero <= 3:
+        return True
+    if numero % 2 == 0 or numero % 3 == 0:
+        return False
+    i = 5
+    while i * i <= numero:
+        if numero % i == 0 or numero % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
+
+soma = 0
+
+for numero in range(1, 2000000):
+    if eh_primo(numero):
+        soma += numero
+
+print(f'A soma de todos os números primos até 2.000.000 é {soma}')
 
 """
 
