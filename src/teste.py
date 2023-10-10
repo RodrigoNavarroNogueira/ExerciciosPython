af93cc6f818a5323e0e9b79c3c8031d339b9ca6b
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-10-09 23:22:09 -0300

    Adicionando script teste

--- src/teste.py
+++ src/teste.py
@@ -1,12 +1,10 @@
-string_numero = "0507"
-numero_inteiro = int(string_numero)
-print(numero_inteiro)  # Isso imprimirá 507
+def contador_divisor(num):
+    divisores = 0
+    for n in range(1, num + 1):
+        if num % n == 0:
+            divisores += 1
+    return divisores
 
-# Se você deseja preservar os zeros à esquerda, você pode fazer o seguinte:
-string_com_zeros_preservados = string_numero  # Armazena a string original
-print(string_com_zeros_preservados)  # Isso imprimirá "0507"
-
-# Se você quiser adicionar zeros à esquerda para um comprimento específico, por exemplo, 6 dígitos:
-comprimento_desejado = 6
-string_com_zeros_preservados = string_numero.zfill(comprimento_desejado)
-print(string_com_zeros_preservados)  # Isso imprimirá "00507"
+num = int(input("Digite um número inteiro: "))
+divisores = contador_divisor(num)
+print(f'Possui {divisores} divisores')
