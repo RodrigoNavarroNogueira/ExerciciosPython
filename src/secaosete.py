f2ddb7d60833e66b02539caf2bc1459133772321
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-10-11 17:27:38 -0300

    Resolvendo 01

--- src/secaosete.py
+++ src/secaosete.py
@@ -1,6 +1,18 @@
 """
 1)
 
+a = []
 
+a.append(1)
+a.append(0)
+a.append(5)
+a.append(-2)
+a.append(-5)
+a.append(7)
+soma = a[0] + a[1] + a[5]
+print(f'A soma é {soma}')
+a.insert(4, 100)
+for n in a:
+    print(n)
 
 """
