b352e66f2e34684a731870ed790e695044618798
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-11-02 23:37:37 -0300

    Resolvendo 01

--- src/secaooito.py
+++ src/secaooito.py
@@ -1,6 +1,19 @@
 """
 1)
 
+def funcao(numero):
+    dobro = numero + numero
+    return dobro
+
+
+def pega_numero():
+    numero = int(input('Digite um numero inteiro: '))
+    return numero
+
+
+numero = pega_numero()
+print(funcao(numero))
 
 """
+
 
