1cbf1cb3c3fce8da717160e984cbbc07cc990f94
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2023-11-07 21:16:56 -0300

    Resolvendo 08

--- src/secaooito.py
+++ src/secaooito.py
@@ -124,6 +124,21 @@ def pega_numero():
 numeros = pega_numero()
 print(funcao(numeros))
 
+8)
+
+def funcao(a, b):
+    a = a ** 2
+    b = b ** 2
+    soma = a + b
+    raiz = soma ** (1/2)
+    return raiz
+
+
+a = int(input('Digite um numero '))
+b = int(input('Digite um numero '))
+print(funcao(a, b))
+
+
 """
 # 976204000
 
